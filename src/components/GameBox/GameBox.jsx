75125ff63a93f0f48e0b0130c3f3ed8ef27d58d8
orshah <orshpov@gmail.com>
2023-07-08 11:17:50 -0500

    added structure

---
+++ src/components/GameBox/GameBox.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function GameBox() {
+  return (
+    <div className="game__box--container">
+      <div className="game__box">
+        <div className="game__box--cell"></div>
+        <div className="game__box--cell"></div>
+        <div className="game__box--cell"></div>
+        <div className="game__box--cell"></div>
+        <div className="game__box--cell"></div>
+        <div className="game__box--cell"></div>
+        <div className="game__box--cell"></div>
+        <div className="game__box--cell"></div>
+        <div className="game__box--cell"></div>
+      </div>
+    </div>
+  );
+}
+
+export default GameBox;
---
+++ src/components/MainGame/MainGame.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import GameBox from "../GameBox/GameBox";
+
+function MainGame() {
+  return <GameBox/>;
+}
+
+export default MainGame;
